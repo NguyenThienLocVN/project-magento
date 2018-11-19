5eb080e9586b0fe4bd11806ea38cc764ca0e18e5
Loc Nguyen <kten1112@gmail.com>
2018-11-20 01:02:09 +0700

    Add "Made to Order" module. Error1: Module 1 not save database Error2: Module 2 Undefined $customerId

---
+++ app/code/local/Company/Mto/Helper/Event.php
@@ -0,0 +1,25 @@
+<?php
+
+class Company_Mto_Helper_Event extends Mage_Core_Helper_Abstract
+{
+    static function cartBeforeSave($observer) {
+        $event = $observer->getEvent();
+        $req = Mage::app()->getRequest();
+        $items = $event->getQuote()->getItemsCollection();
+        $mto_length = $req->get('user_length');
+        $product_id = $req->get('product');
+        if (!$mto_length && !$product_id) {
+        //only run if the user is submitting
+        // data to the cart controller.
+            return;
+        }
+        foreach ($items as $item) {
+            if ($item->getProductId() === $product_id) {
+                if(!$item->getMtoLength()) {
+                    $item->setMtoLength($mto_length);
+                    break;
+                }
+            }
+        }
+    }
+}
---
+++ app/code/local/Company/Mto/sql/mto_setup/mysql4-install-0.1.0.php
@@ -0,0 +1,45 @@
+<?php
+
+$read = Mage::getSingleton('core/resource')->getConnection('core_read');
+
+$eid = $read->fetchRow('select entity_type_id from eav_entity_type where entity_type_code="quote_item"');
+$quote_type_id = $eid['entity_type_id'];
+
+$eid = $read->fetchRow('select entity_type_id from eav_entity_type where entity_type_code="order_item"');
+
+$order_type_id = $eid['entity_type_id'];
+
+$installer = $this;
+$installer->startSetup();
+
+$c = array (
+    'entity_type_id'=>$quote_type_id,
+    'attribute_code'=>'mto_length',
+    'backend_type'=>'varchar',
+    'frontend_input'=>'text',
+    'is_global' => '1',
+    'is_visible' => '0',
+    'is_required' => '0',
+    'is_user_defined' => '1',
+);
+
+$attribute = new Mage_Eav_Model_Entity_Attribute();
+$attribute->loadByCode($c['entity_type_id'],$c['attribute_code'])->setStoreId(0)->addData($c);
+$attribute->save();
+
+$c = array (
+    'entity_type_id'=>$order_type_id,
+    'attribute_code'=>'mto_length',
+    'backend_type'=>'varchar',
+    'frontend_input'=>'text',
+    'is_global' => '1',
+    'is_visible' => '0',
+    'is_required' => '0',
+    'is_user_defined' => '1',
+);
+
+$attribute = new Mage_Eav_Model_Entity_Attribute();
+$attribute->loadByCode($c['entity_type_id'],$c['attribute_code'])->setStoreId(0)->addData($c);
+$attribute->save();
+
+$installer->endSetup();
